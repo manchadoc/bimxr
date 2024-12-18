85db69602e182df3e1eb174117b8219473afd286
manchadoc <manchadoc@unican.es>
2024-12-18 18:42:47 +0100

    Commit 000

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/bimxr/', // Reemplaza 'nombre-del-repo' con el nombre de tu repositorio en GitHub
+  // Otras configuraciones si es necesario
+});
