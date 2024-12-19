9b91a02a7f8a8e9887b03e23a313cd04361559cb
manchadoc <manchadoc@unican.es>
2024-12-19 12:24:35 +0100

    Commit 002

--- src/main.js
+++ src/main.js
@@ -42,9 +42,9 @@ const hitTestSource = await session.requestHitTestSource({ space: viewerSpace })
     reticle.visible = false;
     scene.add(reticle);
   })
-      
+      //https://immersive-web.github.io/webxr-samples/media/gltf/sunflower/sunflower.gltf
   let flower;
-  loader.load("https://immersive-web.github.io/webxr-samples/media/gltf/sunflower/sunflower.gltf", function(gltf) {
+  loader.load("./serc/0.glb", function(gltf) {
     flower = gltf.scene;
   });
   session.addEventListener("select", (event) => {
